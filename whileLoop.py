57a11b994636ac0e4151cc31fbffe4a49a2bf35a
sadrisso <sadrisso2000@gmail.com>
2024-03-16 23:06:19 +0600

    break keyword with while loop

--- whileLoop.py
+++ whileLoop.py
@@ -2,3 +2,17 @@ a = 0
 while a <= 10:
     a = a + 1
     print(a)
+
+x = 10
+while x >= 0:
+    x = x - 1
+    if x == 3:
+        break
+    print(x)
+
+y = 100
+while y >= 0:
+    y = y - 5
+    print(y)
+    if y == 90:
+        break
