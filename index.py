c0a10f70346b9cc2df5dc7e38d6243e5a5ee2821
sadrisso <sadrisso2000@gmail.com>
2024-03-16 15:26:38 +0600

    range function practice

---
+++ index.py
@@ -0,0 +1,14 @@
+
+a = list(range(10))
+print(a)
+
+for i in range(10):
+    print(i)
+
+b = list(range(3, 10))
+c = list(range(5, 10))
+d = list(range(5, 100, 5))
+print(d,c,b)
+
+x = list(range(10, 0, -2))
+print(x)
