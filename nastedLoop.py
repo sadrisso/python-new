5bba6d4168108853e66e50d9cc94ced25ae3b584
sadrisso <sadrisso2000@gmail.com>
2024-03-17 22:50:48 +0600

    nasted loop

---
+++ nastedLoop.py
@@ -0,0 +1,15 @@
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(i*j, end = " ")
+    print()
+
+
+a = 0
+b = 0
+while a <= 5:
+    while b <= 5:
+        a = a+1
+        b = b+1
+        print(a*b, end = " ")
+        print()
