bbb4660448c765054ba0b26d88f276eec2181894
sadrisso <sadrisso2000@gmail.com>
2024-03-18 22:46:55 +0600

    pettern printing basic

---
+++ petternPrinting.py
@@ -0,0 +1,16 @@
+
+for i in range(3):
+    for j in range(i+1):
+        print('#', end = ' ')
+    print()
+
+
+for row in range(10):
+    for col in range(row+1):
+        print('*', end = ' ')
+    print()
+
+for m in range(1, 10):
+    for n in range(m):
+        print('1', end = ' ')
+    print()
