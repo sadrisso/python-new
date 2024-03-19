d26824448da4ff91f3487a068b4518f76b56bf29
sadrisso <sadrisso2000@gmail.com>
2024-03-19 12:53:34 +0600

    problem using loop

---
+++ problem1UsingLoop.py
@@ -0,0 +1,9 @@
+
+a = int(input('Enter a number: '))
+for i in range(1, a+1):
+    print(a, 'X', i, '=', a*i)
+
+
+b = int(input('Enter your number: '))
+for i in range(1, 10):
+    print(b, 'X', i, '=', b*i)
