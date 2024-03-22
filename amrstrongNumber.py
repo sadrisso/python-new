dfbab6f57dfc006375955ffc8f20f11907d73249
sadrisso <sadrisso2000@gmail.com>
2024-03-22 16:39:24 +0600

    reverse a  number

--- amrstrongNumber.py
+++ amrstrongNumber.py
@@ -9,3 +9,14 @@ if int(a) == sum:
     print('Armstrong number')
 else:
     print('Not armstrong number')
+
+
+
+m = int(input('Enter a number: '))
+rev_a = 0
+
+while m>0:
+    last_digit = m%10
+    rev_a = rev_a * 10 + last_digit
+    m //= 10
+print(rev_a)
