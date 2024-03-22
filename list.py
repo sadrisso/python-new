7f7dec370b2e681fe484875b28836750812c1bf4
sadrisso <sadrisso2000@gmail.com>
2024-03-23 00:49:28 +0600

    negetive index printing

--- list.py
+++ list.py
@@ -6,7 +6,26 @@ a[0] = 'changed'
 print(a[0])
 
 
+
 b = [11, 23, 45.5, 'Ami', 'tmi']
 print(b[-3])
 print(b[-1])
 print(b[4])
+
+
+
+b[-3] = 500 # changed in memory
+if 45.5 in b:
+    print('Found')
+else:
+    print('Not found')
+
+
+
+# negetive index printing 
+    
+for i in range(len(b)-1, -1, -1):
+    print(b[i])
+
+for i in range(-1, -len(b)-1, -1):
+    print(b[i])
