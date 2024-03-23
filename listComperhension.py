3ed711418c91ef6fd2d9da2c4d35b448e9f72ed4
sadrisso <sadrisso2000@gmail.com>
2024-03-24 00:01:24 +0600

    list comperhension

---
+++ listComperhension.py
@@ -0,0 +1,15 @@
+
+# it return string formate list
+
+a = input().split()
+print(a)
+
+
+# it return integer formate list
+
+b = list(map(int, input().split()))
+print(b)
+
+
+c = list(map(float, input().split()))
+print(c)
