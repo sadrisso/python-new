a35a7ccaa566d25be5cb78e4f942c66dda979920
sadrisso <sadrisso2000@gmail.com>
2024-03-24 14:43:07 +0600

    list comprehension part1

---
+++ listComprehensionPart1.py
@@ -0,0 +1,17 @@
+
+# iteration through a string using comprehension
+
+a = 'Hello World'
+b = [i for i in a]
+print(b)
+
+
+c = 'Hello Python'
+d = list(c)
+print(d)
+
+e = [i for i in range(1, 20, 2)]
+print(e)
+
+f = list(range(2, 20, 2))
+print(f)
