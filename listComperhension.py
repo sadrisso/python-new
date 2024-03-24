3b7c7c1a2f634af2018483ab6fb234c631e2970a
sadrisso <sadrisso2000@gmail.com>
2024-03-24 14:34:28 +0600

    iteration with list comprehension

--- listComperhension.py
+++ listComperhension.py
@@ -13,3 +13,18 @@ print(b)
 
 c = list(map(float, input().split()))
 print(c)
+
+
+# iteration with list comprehension
+
+x = [10, 20, 30, 40]
+y = [i*2 for i in x]
+print(y)
+
+c = [10, 20, 40, 90]
+d = [i+5 for i in c]
+print(d)
+
+e = [1, 2, 3,4,5]
+f = [i-1 for i in e]
+print(f)
