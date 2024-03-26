e69e45ac7ecca1e69fd63bf4b7fbeb9a1c741480
sadrisso <sadrisso2000@gmail.com>
2024-03-26 22:24:30 +0600

    swaping a list element

--- 2dListComprehension.py
+++ 2dListComprehension.py
@@ -19,3 +19,12 @@ print(a)
 
 res = [[col[row] for col in matrix] for row in range(2)]
 print(res)
+
+
+# swaping a list element
+
+list1 = [1,2,3,4,5,6,7,8,9]
+temp = list1[0]
+list1[0] = list1[-1]
+list1[-1] = temp
+print(list1)
