d4403a6e577ffa576727b01cf468559e5c2bf087
sadrisso <sadrisso2000@gmail.com>
2024-03-29 15:44:00 +0600

    functions basic

---
+++ function.py
@@ -0,0 +1,39 @@
+
+def calculateMean(a, b):
+    mean = (a+b)/2
+    print(mean)
+
+calculateMean(2,2)
+calculateMean(5,5)
+
+
+def calculateGmean(c, d):
+    gmean = (c*d)/(c+d)
+    print(gmean)
+
+calculateGmean(2,3)
+
+
+def even_number_check(e):
+    if e%2 == 0:
+        print('Even number')
+    else:
+        print('Odd number')
+
+even_number_check(2)
+
+
+def bigNumber(m, n):
+    if m>n:
+        print(m,'is greter then', n)
+    else:
+        print('Default')
+
+bigNumber(223,34)
+
+
+def isLesser(w,x):
+    pass
+
+
+    
