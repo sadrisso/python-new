95946bcdc398840c1f6a92ef7f87f5fb0ac3d519
sadrisso <sadrisso2000@gmail.com>
2024-03-30 15:35:13 +0600

    dict & set more

--- dict.py
+++ dict.py
@@ -45,3 +45,57 @@ newStudent = {'sub' : 'math', 'gpa' : 2.44}
 student.update(newStudent)
 
 print(student)
+
+demoDict = {
+    'cat' : 'a small and cute animal',
+    'table' : ('a pice of furniture','list of facts and figure')
+}
+print(demoDict)
+
+
+mrk1 = int(input('Enter 1st sub mark: '))
+mrk2 = int(input('Enter 2nd sub mark: '))
+mrk3 = int(input('Enter 3rd sub mark: '))
+
+allSubs = {}
+allSubs.update({'sub1' : mrk1})
+allSubs.update({'sub2' : mrk2})
+allSubs.update({'sub3' : mrk3})
+print(allSubs)
+
+# sets in python
+
+collection = {1,2,3,3,'hello','pyhton','hello'}
+print(type(collection), collection)
+
+print(len(collection))
+
+# empty sets; syntax
+
+collec = set()
+collec.add((1,2,3,4,5,66,66,66))
+collec.add(1000)
+print(type(collec), collec)
+
+
+set1 = {1,2,3,4,5}
+set2 = {2,3,4,5,7,8}
+print(set1.union(set2))
+print(set1.intersection(set2))
+
+
+subjects = {'java', 'python', 'c++', 'js', 'python', 'js', 'c', 'c', 'php'}
+print(len(subjects))
+
+
+set = {1, 1.0}
+print(set)
+
+
+newSet = {
+    ('float', 1.0),
+    ('int', 1)
+}
+print(newSet)
+
+
