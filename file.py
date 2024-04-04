94c82ef757da43845eaed1fb3a411cee49a82a41
sadrisso <sadrisso2000@gmail.com>
2024-04-04 22:39:10 +0600

    file append

--- file.py
+++ file.py
@@ -5,7 +5,23 @@ print(data)
 print(type(data))
 file.close()
 
+
 f = open('forLoop.py', 'r')
 line1 = f.readline()
 print(line1)
 f.close()
+
+
+x = open('x.txt', 'r+')
+m = x.read()
+y = x.write('Hi')
+print(m)
+print(y)
+x.close()
+
+
+m = open('sample.txt', 'a+')
+appendElement = m.write('I am append')
+readAppend = m.read()
+print(appendElement)
+print(readAppend)
