8139def6992f9ecd5fea1e52949108fa54970a41
sadrisso <sadrisso2000@gmail.com>
2024-04-07 00:56:28 +0600

    property method

--- classMethod.py
+++ classMethod.py
@@ -10,3 +10,23 @@ class Person:
 p1 = Person()
 p1.changeName('Ratul')
 print(Person.name)
+
+
+# property method
+
+class Student:
+    def __init__(self, phy, chm, math):
+        self.phy = phy
+        self.chm = chm
+        self.math = math
+    
+    @property
+    def percentage(self):
+        return str((self.phy + self.chm + self.math) / 3) + '%'
+
+s1 = Student(98, 99, 89)
+print(s1.percentage)
+
+
+
+
