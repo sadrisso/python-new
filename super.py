213d28dd5343cdde451dde4b3d74db31862cba72
sadrisso <sadrisso2000@gmail.com>
2024-04-07 00:03:26 +0600

    use of super method

--- super.py
+++ super.py
@@ -1,1 +1,20 @@
 
+class Student:
+
+    def welcome(self):
+        print('Welcome from student class')
+
+class Teacher:
+    
+    def hiTeacher(self):
+        print('Hi from teacher class')
+
+class Person(Student, Teacher):
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super().welcome()
+        super().hiTeacher()
+
+p1 = Person('Hossain', 32)
