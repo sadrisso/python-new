af7917e93f28c7d42e437172edef3979f1c1ab35
sadrisso <sadrisso2000@gmail.com>
2024-04-06 12:53:06 +0600

    besic oop class

---
+++ oop.py
@@ -0,0 +1,12 @@
+
+class Student:
+    name = 'Karan Sharma'
+    age = 12
+    section = 'A'
+
+s1 = Student()
+print(s1.name, s1.age, s1.section)
+
+s2 = Student()
+print(s2.name, s2.age, s2.section)
+
