cc53149f91b3dc3dfe7cfc6893e07dd5f348809a
sadrisso <sadrisso2000@gmail.com>
2024-04-06 14:32:17 +0600

    class attribute & object attribute

--- oop.py
+++ oop.py
@@ -12,8 +12,17 @@ print(s2.name, s2.age, s2.section)
 
 
 class Teacher:
+
+    collegeName = 'ABC College'
+    name = 'Arjun' # class attribute
+    
+    # default constructor
+    def __init__(self):
+        pass
+
+    # peramitarized constructor
     def __init__(self, name, rank, sallary):
-        self.name = name
+        self.name = name # object attibute
         self.rank = rank
         self.sallary = sallary
         print('adding new student in database: ')
@@ -23,3 +32,23 @@ print(t1.name, t1.rank, t1.sallary)
 
 t2 = Teacher('Amin', 'A', 30000)
 print(t2.name, t2.rank, t2.sallary)
+
+
+
+class Person:
+
+    matches = 45  # class attribute
+    name = 'anonimous'
+
+    def __init__(self, name, age):
+        self.name = name  # object attribute
+        self.age = age
+
+
+p1 = Person('Mushfiq', 40)
+p2 = Person('Shakib', 37)
+p3 = Person('Mahmudullah', 50)
+
+print(p1.name, p1.age, p1.matches)
+print(p2.name, p2.age, p2.matches)
+print(p3.name, p3.age, p3.matches)
