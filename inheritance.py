5d5ebd3842db6b339bb991d8247b20e7c1618800
sadrisso <sadrisso2000@gmail.com>
2024-04-06 20:20:56 +0600

    multiple inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,15 +1,62 @@
-class Car:
+# class Car:
+#     @staticmethod
+#     def start():
+#         print('Car started..')
+
+#     @staticmethod
+#     def stop():
+#         print('Car stoped.')
+
+# class TeslaCar(Car):
+#     def __init__(self, name):
+#         self.name = name
+
+# car1 = TeslaCar('Tcs')
+# print(car1.name, car1.start(), car1.stop())
+
+
+class Bus:
+
     @staticmethod
     def start():
-        print('Car started..')
+        print('Bus started ...')
 
     @staticmethod
     def stop():
-        print('Car stoped.')
+        print('Bus stoped.')
 
-class TeslaCar(Car):
+class Hanif(Bus):
+
+    def __init__(self, type):
+        self.type = type
+
+
+class Type(Hanif):
+
+    def __init__(self, fuel):
+        self.fuel = fuel
+
+
+type1 = Type('diesel')
+bus1 = Hanif('Hino')
+type1.start()
+print(bus1.type)
+print(type1.fuel)
+
+
+# Multiple inheritance
+class A:
+    def welcome(self):
+        print('Welcome from class A')
+
+class B:
+    def hello(self):
+        print('Hello, welcome from class B')
+
+class C(A, B):
     def __init__(self, name):
         self.name = name
 
-car1 = TeslaCar('Tcs')
-print(car1.name, car1.start(), car1.stop())
+c1 = C('himalaya')
+c1.welcome()
+c1.hello()
